8d3c88405777b1b802391598313ff244a3bd9995
tanmehar <thansinazar111@gmail.com>
2021-07-10 18:40:10 +0530

    add new list nodes and retain on clicking add button

--- index.js
+++ index.js
@@ -3,19 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 app.get("/",function(req,res){
     
-    let weekendOrNot = function(currentDay){
-        if(currentDay=="saturday"||currentDay=="sunday"){
-            res.send("it's a weekend");
-        }
-        else{
-            res.send("It's a working day.");
-        }
-    }
-    
-    weekendOrNot("saturday");
+    let today = new Date();
+    let options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+
+    let day = today.toLocaleDateString("en-Us",options);
+
+        res.render("list",{
+            kindDay:day,
+            newListItems:items
+
+        });
 });
+
+app.post("/",function(req,res){
+    let addNew = req.body.addList;
+    items.push(addNew);
+    res.redirect("/");
+})
 
 
 app.listen(3000,function(){
